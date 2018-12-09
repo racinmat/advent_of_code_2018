2fe96227ae01a4b9da54056108fba17b1e3f9086
Matěj Račinský <racinsky.matej@seznam.cz>
2018-12-09 12:24:57 +0100

    another bit of optimization

--- day_9/main.py
+++ day_9/main.py
@@ -14,47 +14,60 @@ def load_rules():
 def play_game(num_players, num_turns):
     marbles = deque([0])
     scores = [0] * num_players
-    curr_player = 1
-    for i in range(1, num_turns):
+    for i in range(1, num_turns + 1):
         if i % 23 == 0:
-            marbles.rotate(8)
-            removed = marbles.popleft()
-            scores[curr_player] += i + removed
+            marbles.rotate(7)
+            removed = marbles.pop()
+            scores[i % num_players] += i + removed
             marbles.rotate(-1)
         else:
             marbles.rotate(-1)
             marbles.append(i)
 
         # some debug printing
-        # marbles_print = ['({})'.format(m) if i == current else str(m) for i, m in enumerate(marbles)]
         # print([curr_player], ''.join([str(m).rjust(4) for m in marbles]))
         # if i % 23 == 0:
         #     print(scores)
-        curr_player = (curr_player + 1) % num_players
-    return scores
+    return max(scores)
+
+
+def play_game_2(max_players, last_marble):
+    scores = [0] * max_players
+    circle = deque([0])
+
+    for marble in range(1, last_marble + 1):
+        if marble % 23 == 0:
+            circle.rotate(7)
+            scores[marble % max_players] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
+
+    return max(scores)
 
 
 def part_1():
-    from time import time
-    start = time()
+    # from time import time
+    # start = time()
     num_players, num_turns = load_rules()
 
-    scores = play_game(num_players, num_turns)
+    max_score = play_game(num_players, num_turns)
 
-    print(max(scores))
-    print(time() - start)
+    print(max_score)
+    # print(time() - start)
 
 
 def part_2():
-    from time import time
-    start = time()
+    # from time import time
+    # start = time()
     num_players, num_turns = load_rules()
     num_turns *= 100
 
-    scores = play_game(num_players, num_turns)
+    max_score = play_game(num_players, num_turns)
 
-    print(max(scores))
-    print(time() - start)
+    print(max_score)
+    # print(time() - start)
 
 
 if __name__ == '__main__':
