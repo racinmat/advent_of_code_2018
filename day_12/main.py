915a58d958dc2501d342b8f360344f2e7b02fa13
MatejRacinsky <matej.racinsky@foxconn4tech.com>
2018-12-12 12:15:01 +0100

    using -1 and 1 instead of 0 and 1

--- day_12/main.py
+++ day_12/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.signal import fftconvolve
 
 def load_plants():
     with open('input.txt', encoding='utf-8') as lines:
@@ -12,35 +12,37 @@ def load_plants():
             line = line.replace('\n', '').replace('#', '1').replace('.', '0')
             rule = list(map(int, line[:5]))
             result = int(line[9:])
+            # skipping rules with 0
+            if result == 0:
+                continue
             rules[tuple(rule)] = result
     return initial, rules
 
 
-def apply_rules_for_generation(plants, rules):
-    new_gen_plants = np.zeros_like(plants)
+# def apply_rules_for_generation(plants, rules):
+#     new_gen_plants = np.zeros_like(plants)
+#
+#     for i in range(2, len(plants) - 2):
+#         part = tuple(plants[i - 2:i + 3])
+#         if part in rules:
+#             new_gen_plants[i] = rules[part]
+#     return new_gen_plants
 
-    for i in range(2, len(plants) - 2):
-        part = tuple(plants[i - 2:i + 3])
-        if part in rules:
-            new_gen_plants[i] = rules[part]
-    return new_gen_plants
 
-
-def apply_rules_for_generation_conv(plants, rules):
-    new_gen_plants = np.zeros_like(plants)
-    rules_np = np.array(list(rules.keys()))
-    rules_results = list(rules.values())
-    for i, rule in enumerate(rules_np):
-        indices = np.where(np.convolve(plants * 2 - 1, (rule * 2 - 1)[::-1], mode='valid') == len(rule))[0]
+def apply_rules_for_generation_conv(plants, rules, rules_results):
+    new_gen_plants = np.ones_like(plants) * -1
+    for i, rule in enumerate(rules):
+        indices = np.where(np.convolve(plants, rule[::-1], mode='valid') == len(rule))[0]
+        # indices = np.where(np.round(fftconvolve(plants, rule[::-1], mode='valid')) == len(rule))[0]
         indices += 2    # to align indices to the middle of array
         new_gen_plants[indices] = rules_results[i]
     return new_gen_plants
 
 
-def calc_next_generation(plants, rules):
-    plants = np.pad(plants, 4, 'constant', constant_values=0)
+def calc_next_generation(plants, rules, rules_np, rules_results):
+    plants = np.pad(plants, 4, 'constant', constant_values=-1)
     # new_gen_plants = apply_rules_for_generation(plants, rules)
-    new_gen_plants = apply_rules_for_generation_conv(plants, rules)
+    new_gen_plants = apply_rules_for_generation_conv(plants, rules_np, rules_results)
     # assert np.allclose(new_gen_plants, new_gen_plants_conv)
     plants_indices = np.where(new_gen_plants == 1)[0]
     plants_start = plants_indices.min()
@@ -51,10 +53,14 @@ def calc_next_generation(plants, rules):
 def part_1():
     initial, rules = load_plants()
     plants = initial
+    plants[plants == 0] = -1
+    rules_np = np.array(list(rules.keys()))
+    rules_np[rules_np == 0] = -1
+    rules_results = list(rules.values())
     # print(''.join(map(str, plants)))
     total_offset = 0
-    for i in range(0, 1000):
-        plants, offset = calc_next_generation(plants, rules)
+    for i in range(0, 10000):
+        plants, offset = calc_next_generation(plants, rules, rules_np, rules_results)
         total_offset += offset
         # print(''.join(map(str, plants)))
     plants_indices = np.where(plants == 1)[0] + total_offset
@@ -65,7 +71,21 @@ def part_1():
 # 187023 is correct answer after 1000 iterations
 
 def part_2():
-    pass
+    initial, rules = load_plants()
+    plants = initial
+    rules_np = np.array(list(rules.keys()))
+    rules_results = list(rules.values())
+    # print(''.join(map(str, plants)))
+    total_offset = 0
+    for i in range(0, 50000000000):
+        if i % 1000000 == 0:
+            print('iter', i)
+        plants, offset = calc_next_generation(plants, rules, rules_np, rules_results)
+        total_offset += offset
+        # print(''.join(map(str, plants)))
+    plants_indices = np.where(plants == 1)[0] + total_offset
+    total_sum = np.sum(plants_indices)
+    print(total_sum)
 
 
 if __name__ == '__main__':
@@ -74,7 +94,7 @@ if __name__ == '__main__':
     start = time()
 
     part_1()
-    part_2()
+    # part_2()
 
     print(time() - start)
 
