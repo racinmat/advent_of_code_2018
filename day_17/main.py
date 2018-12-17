b98234d3c60f5312876112050bb7c54d29fa1afa
MatejRacinsky <matej.racinsky@foxconn4tech.com>
2018-12-17 12:28:15 +0100

    some things not working, but skelet seems ok

--- day_17/main.py
+++ day_17/main.py
@@ -1,9 +1,11 @@
 import re
 import numpy as np
+from scipy import signal
 
 CLAY = -1
 FREE = 1
 WATER = 2
+
 
 def load_input():
     clays = []
@@ -12,9 +14,11 @@ def load_input():
             m = re.match('([xy])=(\d+)\.?\.?(\d+)?, ([xy])=(\d+)\.?\.?(\d+)?', line.replace('\n', ''))
             g = m.groups()
             coord_1 = g[0]
-            range_1 = int(g[1]) if g[2] is None else range(int(g[1]), int(g[2]) + 1)# from inclusive to exclusive range
+            range_1 = int(g[1]) if g[2] is None else range(int(g[1]),
+                                                           int(g[2]) + 1)  # from inclusive to exclusive range
             coord_2 = g[3]
-            range_2 = int(g[4]) if g[5] is None else range(int(g[4]), int(g[5]) + 1)# from inclusive to exclusive range
+            range_2 = int(g[4]) if g[5] is None else range(int(g[4]),
+                                                           int(g[5]) + 1)  # from inclusive to exclusive range
             data = {
                 coord_1: range_1,
                 coord_2: range_2,
@@ -44,19 +48,91 @@ def prepare_data():
     return grid
 
 
-def get_valid_instructions(change_log, instructions):
-    before = change_log['before']
-    instruction = change_log['instruction']
-    instr_num = instruction[0]
-    params = instruction[1:]
-    after = change_log['after']
-    valid_instructions = [instruction for instruction, fun in instructions.items() if fun(params, before) == after]
-    return valid_instructions, instr_num
+def prepare_rules():
+    conditions = [
+        [[WATER],
+         [FREE]],
+        [[FREE, WATER],
+         [CLAY, CLAY]],
+        [[WATER, FREE],
+         [CLAY, CLAY]],
+        [[WATER, FREE],
+         [WATER, WATER]],
+        [[FREE, WATER],
+         [WATER, WATER]],
+        [[FREE, WATER],
+         [CLAY, WATER]],
+        [[WATER, FREE],
+         [WATER, CLAY]],
+        [[WATER, FREE],
+         [CLAY, FREE]],
+        [[FREE, WATER],
+         [FREE, CLAY]],
+    ]
+    results = [
+        [[WATER],
+         [WATER]],
+        [[WATER, WATER],
+         [CLAY, CLAY]],
+        [[WATER, WATER],
+         [CLAY, CLAY]],
+        [[WATER, WATER],
+         [WATER, WATER]],
+        [[WATER, WATER],
+         [WATER, WATER]],
+        [[WATER, WATER],
+         [CLAY, WATER]],
+        [[WATER, WATER],
+         [WATER, CLAY]],
+        [[WATER, WATER],
+         [CLAY, FREE]],
+        [[WATER, WATER],
+         [FREE, CLAY]],
+    ]
+    conv_conditions = [np.flip(np.flip(np.array(c), 0), 1) for c in conditions]  # need to flip for convolution
+    results = [np.array(res) for res in results]
+    return conditions, results, conv_conditions
+
+
+def tick(grid, conditions, results, conv_conditions):
+    matches = []
+    for condition, result in zip(conv_conditions, results):
+        res = signal.convolve2d(grid, condition) == (condition ** 2).sum()
+        indices = np.where(res)
+        if len(indices[0]) > 0:
+            for y, x in zip(*indices):
+                matches.append(((y, x), result))
+
+    # apply only lowest level of changes:
+    max_y = max([indices[0] for indices, result in matches])
+    matches = [(indices, result) for indices, result in matches if indices[0] == max_y]
+
+    for indices, result in matches:
+        y, x = indices
+        y_from, y_to = y - round(result.shape[0] / 2), y - round(result.shape[0] / 2) + result.shape[0]
+        x_from, x_to = x - round(result.shape[1] / 2), x - round(result.shape[1] / 2) + result.shape[1]
+        grid[y_from:y_to, x_from:x_to] = result
+    return grid
+
+
+def print_grid(grid):
+    string_grid = np.where(grid == FREE, '.', '#')
+    string_grid[grid == WATER] = '|'
+
+    [print(''.join(i)) for i in string_grid]
+    print()
 
 
 def part_1():
-    change_logs, instructions, _ = prepare_data()
-    pass
+    old_grid = prepare_data()
+    conditions, results, conv_conditions = prepare_rules()
+    while True:
+        new_grid = tick(old_grid.copy(), conditions, results, conv_conditions)
+        print_grid(new_grid)
+        if np.allclose(old_grid, new_grid):
+            break
+        old_grid = new_grid
+    print(np.sum(new_grid == WATER))
 
 
 def part_2():
