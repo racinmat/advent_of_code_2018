3410b47885f25800fd08c6fa0dc7027f0df70af7
Matěj Račinský <racinsky.matej@seznam.cz>
2018-12-19 21:02:33 +0100

    rewritten input code to the python

--- day_19/main.py
+++ day_19/main.py
@@ -162,131 +162,172 @@ def part_1():
 
     ip = 0
     registry = [0, 0, 0, 0, 0, 0]
-    count = 0
-    while ip < len(program):
-        line = program[ip]
-        registry[ip_pos] = ip
-        instr_name, params = line
-        registry = instructions[instr_name](params, registry)
-        ip = registry[ip_pos]
-        ip += 1
-        count += 1
-        # print(ip, registry)
+    # while ip < len(program):
+    #     line = program[ip]
+    #     registry[ip_pos] = ip
+    #     instr_name, params = line
+    #     registry = instructions[instr_name](params, registry)
+    #     ip = registry[ip_pos]
+    #     ip += 1
+        # print(registry)
+    # [1374, 1, 915, 256, 915, 914]
+    registry = program_in_python(registry)
+    print(registry)
 
-    print(ip, registry)
+
+def program_in_python(r):
+    """"
+    now more shortcutting, based on fixed sequences where you can not jump in the middle
+    init: [1, 0, 0, 0, 0, 0]
+    row 0  goto row 17
+    row 1  r[2] = 1
+    row 2  r[4] = 1
+    row 3  r[1] = r[2] * r[4]
+    row 4  --
+    row 5  if r[1] == r[5] goto row 7 else goto row 8
+    row 6  --
+    row 7  r[0] += r[2]
+    row 8  r[4] += 1
+    row 9  --
+    row 10 if r[4] > r[5] goto row 12 else goto row 3
+    row 11 --
+    row 12 r[2] += 1
+    row 13 --
+    row 14 if r[2] > r[5] goto row 16 else goto row 2
+    row 15 --
+    row 16 goto row 257 (=exit)
+    rows 17-24
+    r[1] = (r[1] + 3) * 22 + 12
+    r[5] = (r[5] + 2)**2 * 19*11 + r[1]
+    row 25 goto row 26 + r[0]   # r[0] is either 0 or 1 now
+    row 26 goto row 1
+    rows 27-33
+    r[1] = 27 * 28 + 59 * 14 * 32
+    r[5] += r[1]
+    row 34 r[0] = 0
+    row 35 goto row 1
+    """
+
+    """
+    r[2] = 1
+    r[4] = 1
+    r[1] = r[2] * r[4]
+    if r[1] == r[5]:
+        r[0] += r[2]
+    r[4] += 1
+    if r[4] > r[5]:
+        r[2] += 1
+        if r[2] > r[5]:
+            return
+        else:
+            r[4] = 1
+            ...
+    else:
+        r[1] = r[2] * r[4]
+        ...
+    """
+
+    r[1] = (r[1] + 3) * 22 + 12
+    r[5] = (r[5] + 2) ** 2 * 19 * 11 + r[1]
+    if r[0] == 1:
+        r[1] = 27 * 28 + 59 * 14 * 32
+        r[5] += r[1]
+        r[0] = 0
+
+    r[2] = 1
+    while r[2] <= r[5]:
+        r[4] = 1
+        while r[4] <= r[5]:
+            r[1] = r[2] * r[4]
+            if r[1] == r[5]:
+                r[0] += r[2]
+            r[4] += 1
+        r[2] += 1
+    return r
 
 
 # 0 wrong
 # 10551314 wrong
 def part_2():
-    # row 0  r[3] = r[3] + 16
-    # row 1  r[2] = 1
-    # row 2  r[4] = 1
-    # row 3  r[1] = r[2] * r[4]
-    # row 4  r[1] = 1 if r[1] == r[5] else 0
-    # row 5  r[3] = r[1] + r[3]
-    # row 6  r[3] = r[3] + 1
-    # row 7  r[0] = r[2] + r[0]
-    # row 8  r[4] = r[4] + 1
-    # row 9  r[1] = 1 if r[4] > r[5] else 0
-    # row 10 r[3] = r[3] + r[1]
-    # row 11 r[3] = 2
-    # row 12 r[2] = r[2] + 1
-    # row 13 r[1] = 1 if r[2] > r[5] else 0
-    # row 14 r[3] = r[1] + r[3]
-    # row 15 r[3] = 1
-    # row 16 r[3] = r[3] * r[3]
-    # row 17 r[5] = r[5] + 2
-    # row 18 r[5] = r[5] * r[5]
-    # row 19 r[5] = r[3] * r[5]
-    # row 20 r[5] = r[5] * 11
-    # row 21 r[1] = r[1] + 3
-    # row 22 r[1] = r[1] * r[3]
-    # row 23 r[1] = r[1] + 12
-    # row 24 r[5] = r[5] + r[1]
-    # row 25 r[3] = r[3] + r[0]
-    # row 26 r[3] = 0
-    # row 27 r[1] = r[3]
-    # row 28 r[1] = r[1] * r[3]
-    # row 29 r[1] = r[3] + r[1]
-    # row 30 r[1] = r[3] + r[1]
-    # row 31 r[1] = r[1] * 14
-    # row 32 r[1] = r[1] * r[3]
-    # row 33 r[5] = r[5] + r[1]
-    # row 34 r[0] = 0
-    # row 35 r[3] = 0
+    """
+    row 0  r[3] = r[3] + 16
+    row 1  r[2] = 1
+    row 2  r[4] = 1
+    row 3  r[1] = r[2] * r[4]
+    row 4  r[1] = 1 if r[1] == r[5] else 0
+    row 5  r[3] = r[1] + r[3]
+    row 6  r[3] = r[3] + 1
+    row 7  r[0] = r[2] + r[0]
+    row 8  r[4] = r[4] + 1
+    row 9  r[1] = 1 if r[4] > r[5] else 0
+    row 10 r[3] = r[3] + r[1]
+    row 11 r[3] = 2
+    row 12 r[2] = r[2] + 1
+    row 13 r[1] = 1 if r[2] > r[5] else 0
+    row 14 r[3] = r[1] + r[3]
+    row 15 r[3] = 1
+    row 16 r[3] = r[3] * r[3]
+    row 17 r[5] = r[5] + 2
+    row 18 r[5] = r[5] * r[5]
+    row 19 r[5] = r[3] * r[5]
+    row 20 r[5] = r[5] * 11
+    row 21 r[1] = r[1] + 3
+    row 22 r[1] = r[1] * r[3]
+    row 23 r[1] = r[1] + 12
+    row 24 r[5] = r[5] + r[1]
+    row 25 r[3] = r[3] + r[0]
+    row 26 r[3] = 0
+    row 27 r[1] = r[3]
+    row 28 r[1] = r[1] * r[3]
+    row 29 r[1] = r[3] + r[1]
+    row 30 r[1] = r[3] + r[1]
+    row 31 r[1] = r[1] * 14
+    row 32 r[1] = r[1] * r[3]
+    row 33 r[5] = r[5] + r[1]
+    row 34 r[0] = 0
+    row 35 r[3] = 0
+    """
 
-    # init: [1, 0, 0, 0, 0, 0]
-    # row 0  goto row 17
-    # row 1  r[2] = 1
-    # row 2  r[4] = 1
-    # row 3  r[1] = r[2] * r[4]
-    # row 4  --
-    # row 5  if r[1] == r[5] goto row 7 else goto row 8
-    # row 6  --
-    # row 7  r[0] += r[2]
-    # row 8  r[4] += 1
-    # row 9  --
-    # row 10 if r[4] > r[5] goto row 12 else goto row 2
-    # row 11 --
-    # row 12 r[2] += 1
-    # row 13 --
-    # row 14 if r[2] > r[5] goto row 16 else goto row 2
-    # row 15 --
-    # row 16 goto row 257 (=exit)
-    # row 17 r[5] += 2
-    # row 18 r[5] = r[5]**2
-    # row 19 r[5] *= 19
-    # row 20 r[5] *= 11
-    # row 21 r[1] += 3
-    # row 22 r[1] *= 22
-    # row 23 r[1] += 12
-    # row 24 r[5] += r[1]
-    # row 25 goto row 26 + r[0]
-    # row 26 goto row 1
-    # row 27 r[1] = 27
-    # row 28 r[1] *= 28
-    # row 29 r[1] += 29
-    # row 30 r[1] += 30
-    # row 31 r[1] *= 14
-    # row 32 r[1] *= 32
-    # row 33 r[5] += r[1]
-    # row 34 r[0] = 0
-    # row 35 goto row 1
+    """
+    init: [1, 0, 0, 0, 0, 0]
+    row 0  goto row 17
+    row 1  r[2] = 1
+    row 2  r[4] = 1
+    row 3  r[1] = r[2] * r[4]
+    row 4  --
+    row 5  if r[1] == r[5] goto row 7 else goto row 8
+    row 6  --
+    row 7  r[0] += r[2]
+    row 8  r[4] += 1
+    row 9  --
+    row 10 if r[4] > r[5] goto row 12 else goto row 3
+    row 11 --
+    row 12 r[2] += 1
+    row 13 --
+    row 14 if r[2] > r[5] goto row 16 else goto row 2
+    row 15 --
+    row 16 goto row 257 (=exit)
+    row 17 r[5] += 2
+    row 18 r[5] = r[5]**2
+    row 19 r[5] *= 19
+    row 20 r[5] *= 11
+    row 21 r[1] += 3
+    row 22 r[1] *= 22
+    row 23 r[1] += 12
+    row 24 r[5] += r[1]
+    row 25 goto row 26 + r[0]
+    row 26 goto row 1
+    row 27 r[1] = 27
+    row 28 r[1] *= 28
+    row 29 r[1] += 29
+    row 30 r[1] += 30
+    row 31 r[1] *= 14
+    row 32 r[1] *= 32
+    row 33 r[5] += r[1]
+    row 34 r[0] = 0
+    row 35 goto row 1
+    """
 
-    # now more shortcutting, based on fixed sequences where you can not jump in the middle
-    # init: [1, 0, 0, 0, 0, 0]
-    # row 0  goto row 17
-    # row 1  r[2] = 1
-    # row 2  r[4] = 1
-    # row 3  r[1] = r[2]
-    # row 4  --
-    # row 5  if r[2] == r[5] goto row 7 else goto row 8
-    # row 6  --
-    # row 7  r[0] += r[5]
-    # row 8  r[4] += 1
-    # row 9  --
-    # row 10 if r[4] > r[5] goto row 12 else goto row 2
-    # row 11 --
-    # row 12 r[2] += 1
-    # row 13 --
-    # row 14 if r[2] > r[5] goto row 16 else goto row 2
-    # row 15 --
-    # row 16 goto row 257 (=exit)
-    # rows 17-24
-    # r[1] = (r[1] + 3) * 22 + 12
-    # r[5] = (r[5] + 2)**2 * 19*11 + r[1]
-    # row 25 goto row 26 + r[0]
-    # row 26 goto row 1
-    # row 27 r[1] = 27
-    # row 28 r[1] *= 28
-    # row 29 r[1] += 29
-    # row 30 r[1] += 30
-    # row 31 r[1] *= 14
-    # row 32 r[1] *= 32
-    # row 33 r[5] += r[1]
-    # row 34 r[0] = 0
-    # row 35 goto row 1
     ip_pos, program, instructions = prepare_data()
 
     ip = 0
@@ -305,7 +346,7 @@ def part_2():
         # ips.append(ip)
         # reg0s.append(registry[0])
         if count % 10000000 == 0:
-            print(ip, registry)
+            print(registry)
         # if count > 1000:
         #     break
 
@@ -317,7 +358,7 @@ def part_2():
     # # plt.plot(np.arange(0, len(values) - 1, 1), np.diff(diff_sums))
     # plt.show()
 
-    print(ip, registry)
+    print(registry)
 
 
 if __name__ == '__main__':
@@ -326,6 +367,6 @@ if __name__ == '__main__':
     start = time()
 
     part_1()
-    part_2()
+    # part_2()
 
     print(time() - start)
