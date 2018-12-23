4a0673c65b1b6e8b0339c7710c65a7821d97dcba
Matěj Račinský <racinsky.matej@seznam.cz>
2018-12-23 12:53:24 +0100

    some analysis on day 23 part 2

--- day_23/main.py
+++ day_23/main.py
@@ -27,7 +27,35 @@ def part_1():
 
 def part_2():
     points, rs = load_input()
-    pass
+    # reaches = np.zeros((np.max(points[:, 0]), np.max(points[:, 1]), np.max(points[:, 2])))
+    # grid_indices = np.array(list(np.ndindex(reaches.shape)))
+    # for point, r in zip(points, rs):
+    #     dists = spatial.distance.cdist(point[np.newaxis, :], grid_indices, metric='cityblock')
+    #     reach = np.where(dists <= r)
+    #     reached_points = grid_indices[reach[1], :][:, 0], grid_indices[reach[1], :][:, 1], grid_indices[reach[1], :][:, 2]
+    #     reaches[reached_points] += 1
+    #
+    # best_point = np.unravel_index(np.argmax(reaches), reaches.shape)
+    # distance = spatial.distance.cityblock([0, 0, 0], best_point)
+    # for point, r in zip(points, rs):
+    #     range_set = set()
+    #     for p0 in range(point[0] - r, point[0] + r + 1):
+    #         remain_dist = r - abs(p0 - point[0])
+    #         for p1 in range(point[1] - remain_dist, point[1] + remain_dist + 1):
+    #             remain_dist = r - (abs(p0 - point[0]) + abs(p1 - point[1]))
+    #             for p2 in range(point[2] - remain_dist, point[2] + remain_dist + 1):
+    #                 range_set.add((p0, p1, p2))
+    #     points_in_range.append(range_set)
+    dense_dists = spatial.distance.pdist(points, metric='cityblock')
+    distances = spatial.distance.squareform(dense_dists)
+    sum_ranges = rs + rs[:, np.newaxis] - np.eye(rs.shape[0]) * rs * 2
+    share_point = distances - sum_ranges
+    share_point_dense = spatial.distance.squareform(share_point)
+
+    print('dont share any point', np.sum(share_point_dense >= 0))
+    print('share some points', np.sum(share_point_dense < 0))
+
+    print()
 
 
 if __name__ == '__main__':
