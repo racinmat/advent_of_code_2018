a29d414fbc7e653d5c8de6aeb4e2356c49acae93
Matěj Račinský <racinsky.matej@seznam.cz>
2018-12-27 01:27:58 +0100

    done day 25 part 1

--- day_25/main.py
+++ day_25/main.py
@@ -6,26 +6,35 @@ from scipy import spatial
 
 def load_input():
     points = []
-    rs = []
     with open('input.txt', encoding='utf-8') as lines:
         for line in lines:
-            m = re.match('pos=<(-?\d+),(-?\d+),(-?\d+)>, r=([- ]?\d+)', line)
-            point = [int(i) for i in m.groups()[0:3]]
-            r = int(m.groups()[3])
+            point = [int(i) for i in line.split(',')]
             points.append(point)
-            rs.append(r)
-    return np.array(points, dtype=np.int32), np.array(rs, dtype=np.int32)
+    return np.array(points, dtype=np.int32)
 
 
+# 571 too high
 def part_1():
-    points, rs = load_input()
-    max_point = points[np.argmax(rs)]
-    dists = spatial.distance.cdist(max_point[np.newaxis, :], points, metric='cityblock')
-    print(np.sum(dists <= np.max(rs)))
+    points = load_input()
+    dense_dists = spatial.distance.pdist(points, metric='cityblock')
+    distances = spatial.distance.squareform(dense_dists)
+    clusters = np.arange(0, distances.shape[0], 1)
+    neighbours_from, neighbours_to = np.where((distances != 0) & (distances <= 3))
+    edges = np.stack((clusters[neighbours_from[neighbours_from < neighbours_to]],
+                      clusters[neighbours_to[neighbours_from < neighbours_to]]))
+
+    # looks like a good union-find algorithm on undirected graph
+    while edges.shape[1] > 0:
+        parent, child = edges[:, 0]
+        edges[edges == clusters[child]] = clusters[parent]
+        clusters[clusters == clusters[child]] = clusters[parent]
+        edges = edges[:, edges[0, :] != edges[1, :]]
+        # merging one edge per iteration
+    print(len(np.unique(clusters)))
 
 
 def part_2():
-    points, rs = load_input()
+    points = load_input()
     # reaches = np.zeros((np.max(points[:, 0]), np.max(points[:, 1]), np.max(points[:, 2])))
     # grid_indices = np.array(list(np.ndindex(reaches.shape)))
     # for point, r in zip(points, rs):
@@ -56,9 +65,9 @@ def part_2():
     # print('share some points', np.sum(share_point_dense < 0))
 
     # mean for prunning estimate
-    centroid = np.mean(points, axis=0).astype(np.int32)
-    in_range = spatial.distance.cdist(centroid[np.newaxis, :], points, metric='cityblock') <= rs
-    np.sum(in_range)
+    # centroid = np.mean(points, axis=0).astype(np.int32)
+    # in_range = spatial.distance.cdist(centroid[np.newaxis, :], points, metric='cityblock') <= rs
+    # np.sum(in_range)
     print()
 
 
